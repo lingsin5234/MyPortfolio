39cd890ede0e82188a138c4046e0cafe8e3593c7
lingsin5234 <sinto.005234@gmail.com>
2020-03-12 10:39:19 -0600

    initial commit after setup of Portfolio Django project

--- resume/admin.py
+++ resume/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import TechSkills, GeneralSkills, Education, WorkExperience, WorkDescriptions, Hobbies
 
-# Register your models here.
+
+admin.site.register(TechSkills)
+admin.site.register(GeneralSkills)
+admin.site.register(Education)
+admin.site.register(WorkExperience)
+admin.site.register(WorkDescriptions)
+admin.site.register(Hobbies)
--- resume/models.py
+++ resume/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
-# Create your models here.
+
+# constants
+MONTHS = (('JAN', 'JAN'), ('FEB', 'FEB'), ('MAR', 'MAR'), ('APR', 'APR'), ('MAY', 'MAY'), ('JUN', 'JUN'),
+          ('JUL', 'JUL'), ('AUG', 'AUG'), ('SEP', 'SEP'), ('OCT', 'OCT'), ('NOV', 'NOV'), ('DEC', 'DEC'))
+
+
+# IT Skills
+class TechSkills(models.Model):
+    name = models.CharField(max_length=25)
+    year_exp = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+# Education
+class Education(models.Model):
+    school = models.CharField(max_length=50)
+    cert = models.CharField(max_length=30)
+    major = models.CharField(max_length=50, null=True)
+    start_year = models.IntegerField()
+    start_month = models.CharField(max_length=3, choices=MONTHS)
+    end_year = models.IntegerField(null=True)
+    end_month = models.CharField(max_length=3, choices=MONTHS, null=True)
+
+    def __str__(self):
+        return self.school
+
+
+# Work Experience
+class WorkExperience(models.Model):
+    company = models.CharField(max_length=50)
+    title = models.CharField(max_length=20)
+    start_year = models.IntegerField()
+    start_month = models.CharField(max_length=3, choices=MONTHS)
+    end_year = models.IntegerField(null=True)
+    end_month = models.CharField(max_length=3, choices=MONTHS, null=True)
+
+    def __str__(self):
+        return self.company + self.title
+
+
+# Work Descriptions
+class WorkDescriptions(models.Model):
+    job = models.ForeignKey(WorkExperience, on_delete=models.SET_NULL, null=True, default=None)
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.job
+
+
+# Hobbies
+class Hobbies(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+
+# General Skills
+class GeneralSkills(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
