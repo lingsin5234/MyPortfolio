abb07defe0e1a680edd3ba1c22e87a8a105bf07d
lingsin5234 <sinto.005234@gmail.com>
2020-03-22 10:51:43 -0600

    updated menu and README link

--- resume/menus.py
+++ resume/menus.py
@@ -2,11 +2,11 @@
 from menu import Menu, MenuItem
 from django.urls import reverse
 from . import views as resume_vw
-from djangoapps import settings as st
 
 
 # add items to the main menu
 Menu.add_item("main", MenuItem("Home", reverse(resume_vw.homepage), weight=10))
-Menu.add_item("main", MenuItem("Resume", reverse(resume_vw.show_resume), weight=10))
+Menu.add_item("main", MenuItem("My Resume", reverse(resume_vw.show_resume), weight=10))
+Menu.add_item("main", MenuItem("Project Info", reverse(resume_vw.project_markdown), weight=10))
 Menu.add_item("main", MenuItem("About Me", reverse(resume_vw.about_me), weight=10))
 Menu.add_item("main", MenuItem("Contact Me", reverse(resume_vw.contact_me), weight=10))
