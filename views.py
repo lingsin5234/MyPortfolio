2a6c96dee4110188fbb351cb68adb36662667fa6
lingsin5234 <sinto.005234@gmail.com>
2020-03-27 15:23:22 -0600

    weather data marked coming soon

--- views.py
+++ views.py
@@ -19,7 +19,7 @@ def homepage(request):
         {'id': 3, 'name': 'Budget Demo', 'img': 'static/img/piggybank.jpg', 'link': 'budget/'},
         {'id': 4, 'name': 'Wordpress - Softball Team', 'img': 'static/img/looneytoons.jpg',
             'link': 'https://looneytoons.sinto-ling.ca'},
-        {'id': 5, 'name': 'Weather Data', 'img': 'static/img/noaa.jpg', 'link': '/'},
+        {'id': 5, 'name': 'Coming Soon!', 'img': 'static/img/noaa.jpg', 'link': '/'},
         # {'id': 6, 'name': 'Wordpress - My Travels', 'img': 'static/img/looneytoons.jpg',
         #  'link': 'https://travel.sinto-ling.ca'},
     ]
